98971d896553e5dca9411e8cec33083487170b34
Cyanosite <38476845+Cyanosite@users.noreply.github.com>
2020-12-29 22:42:49 +0100

    Update day3.py

--- day_3/day3.py
+++ day_3/day3.py
@@ -1,7 +1,7 @@
 slopes = {(1, 1): 0, (3, 1): 0, (5, 1): 0, (7, 1): 0, (1, 2): 0}
 for key, value in slopes.items():
     temp = 0
-    for i, item in enumerate(open('source.txt', 'r')):
+    for i, item in enumerate(open('./day_3/source.txt', 'r')):
         if i % key[1] != 0:
             continue
         else:
@@ -14,10 +14,11 @@ for key, value in slopes.items():
                 if item[temp] == '#':
                     slopes[key] += 1
             temp += key[0]
+print(f'Part1: {slopes[(3, 1)]}')
 results = []
 final = 1
 for value in slopes.values():
     results.append(value)
 for i in range(len(results)):
     final *= results[i]
-print(final)
+print(f'Part2: {final}')
