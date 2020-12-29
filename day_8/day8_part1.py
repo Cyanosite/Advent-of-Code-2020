98442f34796da4e9d4f7cfd4f90b156f4fac0a01
Cyanosite <38476845+Cyanosite@users.noreply.github.com>
2020-12-29 22:22:54 +0100

    Update day8_part1.py

--- day_8/day8_part1.py
+++ day_8/day8_part1.py
@@ -1,24 +1,24 @@
-acc=0
-ran=[]
+acc = 0
+ran = []
 instruction = []
 inst_num = []
-with open('source.txt', 'r') as source:
+with open('./day_8/source.txt', 'r') as source:
     for line in source:
-        line=line.strip().split()
+        line = line.strip().split()
         instruction.append(line[0])
         inst_num.append(line[1])
-i=0
+i = 0
 ran.append(len(instruction))
-while(True):
-    print(i)
+while(i not in ran):
     if instruction[i] == 'nop':
-        i+=1
+        i += 1
         continue
     elif instruction[i] == 'acc':
         ran.append(i)
-        acc+=int(inst_num[i])
-        i+=1
+        acc += int(inst_num[i])
+        i += 1
         continue
     elif instruction[i] == 'jmp':
-        i+=int(inst_num[i])
+        i += int(inst_num[i])
         continue
+print(f"The value of the accumulator: {acc}")
