4ca82ee0be8d8f1433c8ecc53a470e22d3e596ba
Cyanosite <38476845+Cyanosite@users.noreply.github.com>
2020-12-29 22:42:33 +0100

    Update day1.py

--- day_1/day1.py
+++ day_1/day1.py
@@ -1,10 +1,12 @@
-numbers = []
-for i in open('source.txt'):
-    numbers.append(int(i.strip()))
+numbers = [int(i) for i in open('./day_1/source.txt').read().strip().split()]
+part1 = set()
+part2 = set()
 for i in numbers:
     for s in numbers:
         if i + s == 2020:
-            print(i * s)
+            part1.add(i * s)
         for n in numbers:
             if i + s + n == 2020:
-                print(i * s * n)
+                part2.add(i * s * n)
+print(f'Part1: {part1}')
+print(f'Part2: {part2}')
