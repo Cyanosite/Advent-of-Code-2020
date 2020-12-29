94807f33122e8a1f037a5e009930a658947d553b
Cyanosite <38476845+Cyanosite@users.noreply.github.com>
2020-12-29 22:29:05 +0100

    Update day6_optimized.py

--- day_6/day6_optimized.py
+++ day_6/day6_optimized.py
@@ -9,9 +9,9 @@ def getCommonAnswers(answer):
     return len(set(answer[0]).intersection(*answer))
 
 
-for i in open('source.txt').read().split('\n\n'):
+for i in open('./day_6/source.txt').read().split('\n\n'):
     answers.append(getAnswers(i.replace('\n', '')))
     common_answers.append(getCommonAnswers(i.strip().split('\n')))
 
-print(sum(answers))
-print(sum(common_answers))
+print(f'Part1: {sum(answers)}')
+print(f'Part2: {sum(common_answers)}')
