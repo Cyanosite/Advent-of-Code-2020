60671d73077d29d2d046540eb6247d714b69ed6e
Cyanosite <38476845+Cyanosite@users.noreply.github.com>
2020-12-30 14:12:10 +0100

    Update day7.py

--- day_7/day7.py
+++ day_7/day7.py
@@ -5,21 +5,19 @@ bags = {}
 with open('./day_7/source.txt', 'r') as source:
     for line in source:
         line = line.strip().split()
-        bags[f"{line[0]} {line[1]}"] = []
+        bags[f"{line[0]} {line[1]}"] = {}
         for i, item in enumerate(line):
             if i <= 3:
                 continue
             if i % 4 == 0:
-                bags[f"{line[0]} {line[1]}"].append(
-                    {f"{line[i + 1]} {line[i + 2]}": item})
+                bags[f"{line[0]} {line[1]}"][f"{line[i + 1]} {line[i + 2]}"] = item
 #  PART 1
 
 
 def contain(bag):
     shiny = False
     if bag in bags.keys():
-        for items in bags[bag]:
-            for key in items.keys():
+            for key in bags[bag].keys():
                 if key == 'shiny gold':
                     return 1
                 elif key == 'other bags.':
